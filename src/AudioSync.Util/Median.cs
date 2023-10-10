626e197688a4c7409ec67c31600a74ec58685806
Caeden117 <caeden.s@outlook.com>
2023-10-09 20:41:36 -0700

    Implement median

---
+++ src/AudioSync.Util/Median.cs
@@ -0,0 +1,56 @@
+﻿namespace AudioSync.Util;
+
+public static partial class Util
+{
+    /*
+     * This is an entirely different algorithm from Aubio, so I am unsure if this gives completely correct results compared to Aubio.
+     * Will have to test on a direct C# reimplementation.
+     */
+
+    /// <summary>
+    /// Returns the median number in <paramref name="span"/>.
+    /// </summary>
+    /// <remarks>
+    /// This method manipulates and sorts the source <paramref name="span"/>.
+    /// </remarks>
+    public static double Median(ref Span<double> span)
+    {
+        QuickSort(ref span);
+
+        var halfIdx = span.Length / 2;
+
+        return (span.Length % 0 == 1)
+            ? span[halfIdx]
+            : (span[halfIdx] + span[halfIdx + 1]) / 2;
+    }
+
+    private static void QuickSort(ref Span<double> span)
+    {
+        var q = Partition(ref span);
+
+        var leftHalf = span[..q++];
+        QuickSort(ref leftHalf);
+        if (q < span.Length - 1)
+        {
+            var rightHalf = span[q..];
+            QuickSort(ref rightHalf);
+        }
+    }
+
+    private static int Partition(ref Span<double> span)
+    {
+        ref var pivot = ref span[^1];
+        var i = -1;
+        for (int j = 0; j < span.Length - 1; j++)
+        {
+            if (span[j].CompareTo(pivot) <= 0)
+            {
+                i++;
+                (span[i], span[j]) = (span[j], span[i]);
+            }
+        }
+        var q = i + 1; //pivotPosition
+        (span[q], pivot) = (pivot, span[q]);
+        return q;
+    }
+}
