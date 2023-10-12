8f374fe9f891841d06fbb3bf5825fade037ae870
Caeden117 <caeden.s@outlook.com>
2023-10-11 18:10:42 -0700

    whoops forgot about this

--- src/AudioSync.Util/Levels.cs
+++ src/AudioSync.Util/Levels.cs
@@ -5,7 +5,7 @@ public static partial class Utils
     /// <summary>
     /// Gets the average linear level of the given <paramref name="span"/>.
     /// </summary>
-    public static double LevelLinear(Span<double> span)
+    public static double LevelLinear(in Span<double> span)
     {
         var energy = 0.0;
         for (var i = 0; i < span.Length; i++)
@@ -18,12 +18,12 @@ public static partial class Utils
     /// <summary>
     /// Gets the average sound pressure level of the given <paramref name="span"/> in db.
     /// </summary>
-    public static double DBSoundPressureLevel(Span<double> span)
-        => 10.0 * Math.Log10(LevelLinear(span));
+    public static double DBSoundPressureLevel(in Span<double> span)
+        => 10.0 * Math.Log10(LevelLinear(in span));
 
     /// <summary>
     /// Determines if the given <paramref name="span"/> is "silent", if the average sound pressure level is below the given <paramref name="threshold"/>.
     /// </summary>
-    public static bool IsSilence(Span<double> span, double threshold)
-        => DBSoundPressureLevel(span) < threshold;
+    public static bool IsSilence(in Span<double> span, double threshold)
+        => DBSoundPressureLevel(in span) < threshold;
 }
