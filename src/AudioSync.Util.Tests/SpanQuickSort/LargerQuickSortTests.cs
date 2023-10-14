54a7fa02e704692fc15a5151a189b3eafd66ba7d
Caeden117 <caeden.s@outlook.com>
2023-10-13 18:09:19 -0700

    add Quick Sort tests for larger collections

---
+++ src/AudioSync.Util.Tests/SpanQuickSort/LargerQuickSortTests.cs
@@ -0,0 +1,33 @@
+﻿namespace AudioSync.Util.Tests.SpanQuickSort;
+
+[TestFixture(100)]
+[TestFixture(1000)]
+[TestFixture(10000)]
+[TestFixture(100000)]
+// Testing Quick Sort at larger scales with data filled by RNG
+public class LargerQuickSortTests
+{
+    private readonly Random rng = new();
+    private readonly double[] numbers;
+
+    public LargerQuickSortTests(int n)
+    {
+        numbers = new double[n];
+
+        // Fill with RNG data
+        for (var i = 0; i < n; i++)
+        {
+            numbers[i] = rng.Next();
+        }
+    }
+
+    [Test]
+    public void Sort()
+    {
+        var span = numbers.AsSpan();
+
+        Utils.QuickSort(ref span);
+
+        Assert.That(numbers, Is.Ordered);
+    }
+}
