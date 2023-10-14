44e2e6b41f501eae9bd813a3acbd66e65d4d4d3d
Caeden117 <caeden.s@outlook.com>
2023-10-13 17:45:36 -0700

    Publicize Quick Sort for testing (and other usage i guess)

--- src/AudioSync.Util/Median.cs
+++ src/AudioSync.Util/Median.cs
@@ -38,48 +38,18 @@ public static partial class Utils
             : (span[halfIdx - 1] + span[halfIdx]) / 2;
     }
 
-#if NET7_0_OR_GREATER
-    /// <summary>
-    /// Returns the median number in <paramref name="span"/>.
-    /// </summary>
-    /// <remarks>
-    /// This method stack allocates a copy of <paramref name="span"/> to perform work, so the original <paramref name="span"/> is not modified.
-    /// </remarks>
-    public static T SafeMedian<T>(in Span<double> span) where T : unmanaged, INumber<T>, IDivisionOperators<T, int, T>
-    {
-        Span<double> work = stackalloc T[span.Length];
-        span.CopyTo(work);
-
-        return Median(ref work);
-    }
-
-    /// <summary>
-    /// Returns the median number in <paramref name="span"/>.
-    /// </summary>
-    /// <remarks>
-    /// This method manipulates and sorts the source <paramref name="span"/>.
-    /// </remarks>
-    public static T Median<T>(ref Span<T> span) where T : INumber<T>, IDivisionOperators<T, int, T>
-    {
-        Debug.Assert(span.Length > 0, $"{nameof(span)} must have items to take the median of.");
-
-        QuickSort(ref span);
-
-        var halfIdx = span.Length / 2;
-
-        return (span.Length % 2 == 1)
-            ? span[halfIdx]
-            : (span[halfIdx - 1] + span[halfIdx]) / 2;
-    }
-#endif
-
     /*
      * I did some basic benchmarking between various methods of finding median: MathNET.Numerics, Nth Order Statistics, and the method
      * that seemed to be the fastest... sort and pick median.
      * 
-     * .NET doesn't seem to have sort methods for Spans, so here's a quick implementation of Quick Sort (haha) for Span
+     * .NET Standard 2.1 doesn't seem to have sort methods for Spans, so here's a quick implementation of Quick Sort (haha) for Span
      */
-    private static void QuickSort<T>(ref Span<T> span) where T : IComparable<T>
+    /// <summary>
+    /// A Quick Sort implementation for <see cref="Span{T}"/>, offering sorting capabilities for collections on both the heap and the stack.
+    /// </summary>
+    /// <typeparam name="TComparable">Comparable type.</typeparam>
+    /// <param name="span">Span to sort.</param>
+    public static void QuickSort<TComparable>(ref Span<TComparable> span) where TComparable : IComparable<TComparable>
     {
         if (span.Length < 2) return;
 
