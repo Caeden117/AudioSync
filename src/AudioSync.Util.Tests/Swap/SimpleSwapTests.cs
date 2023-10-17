513e8f0ccd31984e32975f9dcb7dbd685f8fb83a
Caeden117 <caeden.s@outlook.com>
2023-10-16 22:00:36 -0700

    Cover exception cases in Swap

---
+++ src/AudioSync.Util.Tests/Swap/SimpleSwapTests.cs
@@ -0,0 +1,29 @@
+﻿using AudioSync.Util.Exceptions;
+
+namespace AudioSync.Util.Tests.Swap;
+
+// Simple tests of Swap that do not need multiple executions (exception/unhappy paths to keep track of)
+internal class SimpleSwapTests
+{
+    [Test]
+    public void ThrowsOnOneElement()
+    {
+        Assert.Throws<AudioSyncFatalException>(() =>
+        {
+            Span<double> span = stackalloc double[] { 1.0 };
+
+            Utils.Swap(ref span);
+        });
+    }
+
+    [Test]
+    public void ThrowsOnEmpty()
+    {
+        Assert.Throws<AudioSyncFatalException>(() =>
+        {
+            Span<double> span = stackalloc double[0];
+
+            Utils.Swap(ref span);
+        });
+    }
+}
