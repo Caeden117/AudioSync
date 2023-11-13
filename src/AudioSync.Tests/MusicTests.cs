d604e691f79744b4406b4800c2c9df0070912ba6
Caeden117 <caeden.s@outlook.com>
2023-11-12 22:40:44 -0800

    ok i give up just use linux folder separator instead (windows supports this anyways)

--- src/AudioSync.Tests/MusicTests.cs
+++ src/AudioSync.Tests/MusicTests.cs
@@ -6,13 +6,13 @@ namespace AudioSync.Tests;
 // All test songs *MUST* use ogg!
 // NAudio mp3 support is Windows exclusive.
 
-[TestFixture("Music\\Creo\\Place on Fire.ogg", 70)] // Creo - Place on Fire @ 70 BPM
-[TestFixture("Music\\Creo\\Rock Thing.ogg", 114.0)] // Creo - Rock Thing @ 114 BPM
-[TestFixture("Music\\Creo\\Showdown.ogg", 110.0)] // Creo - Showdown @ 110 BPM
-[TestFixture("Music\\Creo\\Ahead of the Curve.ogg", 96.0)] // Creo - Rock Thing @ 96 BPM
-[TestFixture("Music\\Creo\\Sphere.ogg", 100.0)] // Creo - Sphere @ 100 BPM
-[TestFixture("Music\\Creo\\Dimension.ogg", 115.0)] // Creo - Dimension @ 115 BPM
-[TestFixture("Music\\Creo\\322.ogg", 128.0)] // Creo - 322 @ 128 BPM
+[TestFixture("Music/Creo/Place on Fire.ogg", 70)] // Creo - Place on Fire @ 70 BPM
+[TestFixture("Music/Creo/Rock Thing.ogg", 114.0)] // Creo - Rock Thing @ 114 BPM
+[TestFixture("Music/Creo/Showdown.ogg", 110.0)] // Creo - Showdown @ 110 BPM
+[TestFixture("Music/Creo/Ahead of the Curve.ogg", 96.0)] // Creo - Rock Thing @ 96 BPM
+[TestFixture("Music/Creo/Sphere.ogg", 100.0)] // Creo - Sphere @ 100 BPM
+[TestFixture("Music/Creo/Dimension.ogg", 115.0)] // Creo - Dimension @ 115 BPM
+[TestFixture("Music/Creo/322.ogg", 128.0)] // Creo - 322 @ 128 BPM
 public class MusicTests
 {
     private readonly SyncAnalyser syncAnalyser;
@@ -31,7 +31,7 @@ public class MusicTests
     public void CalculateSongBPM()
     {
         // Read mono samples
-        var reader = new VorbisWaveReader(Path.GetFullPath(song));
+        var reader = new VorbisWaveReader(song);
         var channels = reader.WaveFormat.Channels;
         var sampleLength = (int)Math.Ceiling(reader.TotalTime.TotalSeconds * reader.WaveFormat.AverageBytesPerSecond);
 
